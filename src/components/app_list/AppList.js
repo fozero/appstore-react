720a74876e106b3b61214666f1b488ebc0f37f6d
fozero <fozero@126.com>
2019-07-09 21:26:43 +0800

    update

--- src/components/app_list/AppList.js
+++ src/components/app_list/AppList.js
@@ -11,7 +11,9 @@ class AppList extends Component {
                             return (
                                 <li className='list-item' key={index}>
                                     <div className='app-index'>{index+1}</div>
-                                    <img className='app-icon' src={item['im:image'][0].label} alt="" />
+                                    <div className='app-img'>
+                                        <img className='app-icon' src={item['im:image'][0].label} alt="" />
+                                    </div>
                                     <div className='app-info'>
                                         <div className='app-name'>{item['im:name'].label}</div>
                                         <div className='app-categray'>{item.category.attributes.label}</div>
